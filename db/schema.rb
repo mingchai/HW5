6f581f64271b56d1d01686f00838a7294873688f
Ming Chai <ming.chai4@gmail.com>
2018-12-23 20:01:50 -0800

    add Admin user feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_21_211150) do
+ActiveRecord::Schema.define(version: 2018_12_24_035117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2018_12_21_211150) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false
   end
 
   add_foreign_key "comments", "posts"
